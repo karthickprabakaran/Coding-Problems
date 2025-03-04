d47edbf1f5bc7670eeba020d39f0af295790b8a6
karthickprabakaran <166775653+karthickprabakaran@users.noreply.github.com>
2025-03-04 19:40:58 +0530

    Time: 3 ms (7.24%), Space: 62 MB (18.51%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,27 +1,17 @@
 class Solution {
     public int maxProfit(int[] prices) {
-
-        // declare the min, max and the length of the array
-
-        // loop through the array and calculate the cost by subracting each element of the prices array
-        //with the minimum element
-
-        //compare the cost and the current max value and replace accordingly 
+        
 
         int n = prices.length;
+        int mini = prices[0];
+        int profit = 0;
 
-        int min = prices[0];
-        int max = 0;
-
-        for(int i=0;i<n;i++)
+        for(int i=1;i<n;i++)
         {
-            int cost = prices[i] - min;
-
-            max = Math.max(cost, max);
-            min = Math.min(min, prices[i]);
-        }  
-
-        return max;
-        
+            int cost = prices[i] - mini;
+            profit = Math.max(cost,profit);
+            mini = Math.min(mini, prices[i]);
+        }
+        return profit;
     }
 }
