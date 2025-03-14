30a6d5a30ff7cd0bf157ceadec1dba73bbd8737f
karthickprabakaran <166775653+karthickprabakaran@users.noreply.github.com>
2025-03-14 13:35:43 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Find if +ve, -ve or 0/find-if-ve-ve-or-0.c
@@ -0,0 +1,29 @@
+//{ Driver Code Starts
+// Initial Template for C
+
+// } Driver Code Ends
+
+#include <stdio.h>
+
+int main() {
+    // code here
+    int a;
+    scanf("%d", &a);
+    if(a > 0)
+    {
+        printf("Positive");
+    }
+    else if( a == 0)
+    {
+        printf("Zero");
+    }
+    else{
+        printf("Negative");
+    }
+    return 0;
+}
+
+
+//{ Driver Code Starts.
+
+// } Driver Code Ends
