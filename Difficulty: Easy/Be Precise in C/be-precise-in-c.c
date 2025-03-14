ba0cdffa4c15456820b196568f7ead7d4ac1ebef
karthickprabakaran <166775653+karthickprabakaran@users.noreply.github.com>
2025-03-14 13:33:14 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Be Precise in C/be-precise-in-c.c
@@ -0,0 +1,21 @@
+//{ Driver Code Starts
+
+// } Driver Code Ends
+
+#include <stdio.h>
+
+int main() {
+    float a, b;
+    scanf("%f %f", &a, &b);
+
+    // print the value of a/b up to 3 place precision after decimal point
+    // code here
+    float result = a / b ;
+    printf("%.3f", result);
+    return 0;
+}
+
+
+//{ Driver Code Starts.
+
+// } Driver Code Ends
