179ac34b5f6347d4f6f46c7639f3cf71a1c98d14
karthickprabakaran <166775653+karthickprabakaran@users.noreply.github.com>
2025-03-14 13:34:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Compare two numbers/compare-two-numbers.c
@@ -0,0 +1,26 @@
+//{ Driver Code Starts
+// Initial Template for C
+
+// } Driver Code Ends
+
+// User function Template for C
+#include <stdio.h>
+
+int main() {
+    // code here
+    int a,b;
+    scanf("%d %d", &a,&b);
+    if(a > b)
+    {
+        printf("1");
+    }
+    else{
+        printf("0");
+    }
+    return 0;
+}
+
+
+//{ Driver Code Starts.
+
+// } Driver Code Ends
