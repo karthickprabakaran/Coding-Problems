3ce1c30acbff75b256ed25db5cc1910065986548
karthickprabakaran <166775653+karthickprabakaran@users.noreply.github.com>
2025-03-14 13:37:13 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Sum of N Numbers/sum-of-n-numbers.c
@@ -0,0 +1,25 @@
+//{ Driver Code Starts
+// Initial Template for C
+
+// } Driver Code Ends
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    // code here
+    int sum = 0;
+    for(int i=1;i<=n;i++)
+    {
+        sum+=i;
+    }
+    printf("%d", sum);
+    return 0;
+}
+
+
+//{ Driver Code Starts.
+
+// } Driver Code Ends
