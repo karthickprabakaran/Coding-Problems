c794791d94920df555114b6c3351196a04095f1d
karthickprabakaran <166775653+karthickprabakaran@users.noreply.github.com>
2025-03-30 21:51:26 +0530

    Time: 0 ms (100%), Space: 40.4 MB (92.28%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,21 +1,13 @@
-
 class Solution {
     public boolean isHappy(int n) {
-        Set<Integer> usedIntegers = new HashSet<>();
-        
-        while (n != 1 && !usedIntegers.contains(n)) {
-            usedIntegers.add(n);
-            int sum = 0;
-
-            while (n != 0) {
-                int digit = n % 10;
-                sum += digit * digit; // No need for Math.pow
-                n = n / 10;
-            }
-
-            n = sum;
-        }
-
-        return n == 1;
+       while (n >= 10 || n == 7) {
+			int sum = 0;
+			while (n > 0) {
+				sum += (n % 10) * (n % 10);
+				n /= 10;
+			}
+			n = sum;
+		}
+		return n == 1;
     }
 }
