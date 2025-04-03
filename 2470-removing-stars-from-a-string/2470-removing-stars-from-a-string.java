447b137ecd5a6d4eb29e4dfd849903e070c43ccd
karthickprabakaran <166775653+karthickprabakaran@users.noreply.github.com>
2025-04-03 16:24:05 +0530

    Time: 13 ms (96.48%), Space: 45.7 MB (80.59%) - LeetHub

--- 2470-removing-stars-from-a-string/2470-removing-stars-from-a-string.java
+++ 2470-removing-stars-from-a-string/2470-removing-stars-from-a-string.java
@@ -1,21 +1,19 @@
-    class Solution {
-        public String removeStars(String s) {
-            StringBuffer sb = new StringBuffer("");
+class Solution {
+    public String removeStars(String s) {
+        char[] sb = s.toCharArray(); // Fixed size array
+        int index = 0; // Pointer to track valid characters
 
-            for(char ch : s.toCharArray())
-            {
-                if(ch == '*')
-                {
-                    if(sb.length() > 0)
-                    {
-                        sb.deleteCharAt(sb.length() -1);
-                    }
+        for (char ch : s.toCharArray()) {
+            if (ch == '*') {
+                if (index > 0) {
+                    index--; // Remove the last character
                 }
-                else{
-                    sb.append(ch);
-                }
+            } else {
+                sb[index] = ch; // Store character at index
+                index++;
             }
-
-            return sb.toString();
         }
+
+        return new String(sb, 0, index); // Convert only valid characters to string
     }
+}
