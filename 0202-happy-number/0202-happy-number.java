b4692b1d31de59ea784f0564f7fab3822dede2fa
karthickprabakaran <166775653+karthickprabakaran@users.noreply.github.com>
2025-04-09 16:06:27 +0530

    Time: 0 ms (100%), Space: 41.1 MB (20.86%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,27 +1,28 @@
 class Solution {
     public boolean isHappy(int n) {
-        
         int slow = n;
         int fast = n;
 
         do{
             slow = findSquare(slow);
             fast = findSquare(findSquare(fast));
-        }while(fast != slow);
+        }while(slow != fast);
 
         if(slow == 1) return true;
 
         return false;
     }
 
-    private static int findSquare(int n)
+    private int findSquare(int n)
     {
-        int sum = 0;
-        while(n != 0){
+        int ans = 0;
+        while(n > 0)
+        {
             int digit = n %10;
-            sum += digit * digit;
+            ans += digit * digit;
             n = n/10;
         }
-        return sum;
+
+        return ans;
     }
 }
