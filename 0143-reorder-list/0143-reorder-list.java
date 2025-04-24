532d327bfdc38f58ca1250694e80587480842b25
karthickprabakaran <166775653+karthickprabakaran@users.noreply.github.com>
2025-04-24 05:50:00 +0530

    Time: 2 ms (85.63%), Space: 48.6 MB (23.33%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.java
+++ 0143-reorder-list/0143-reorder-list.java
@@ -1,32 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public void reorderList(ListNode head) {
-        if (head == null || head.next == null) return;
 
-        // Step 1: Find the middle
         ListNode mid = getMid(head);
 
-        // Step 2: Reverse the second half
         ListNode secondHead = reverse(mid.next);
-        mid.next = null; // Important: split the list into two halves
 
-        // Step 3: Merge the two halves
-        while (head != null && secondHead != null) {
+        mid.next = null;
+
+        while(head != null && secondHead != null)
+        {
             ListNode temp = head.next;
             head.next = secondHead;
             head = temp;
 
-            temp = secondHead.next;
+            ListNode temp1 = secondHead.next;
             secondHead.next = head;
-            secondHead = temp;
+            secondHead = temp1;
         }
-        if(head != null) head=null;
+        
     }
 
-    private ListNode getMid(ListNode head) {
+    private static ListNode getMid(ListNode head)
+    {
         ListNode slow = head;
-        ListNode fast = head.next;
+        ListNode fast = head;
 
-        while (fast != null && fast.next != null ) {
+        while(fast!= null && fast.next != null)
+        {
             slow = slow.next;
             fast = fast.next.next;
         }
@@ -34,11 +44,14 @@ class Solution {
         return slow;
     }
 
-    private ListNode reverse(ListNode head) {
+
+    private static ListNode reverse(ListNode head)
+    {
         ListNode prev = null;
         ListNode current = head;
 
-        while (current != null) {
+        while(current != null)
+        {
             ListNode next = current.next;
             current.next = prev;
             prev = current;
