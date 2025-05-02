404c1245a6453dc567691c426d4994382c24250e
karthickprabakaran <166775653+karthickprabakaran@users.noreply.github.com>
2025-05-02 07:09:10 +0530

    Time: 0 ms (100%), Space: 41.5 MB (82.56%) - LeetHub

--- 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.java
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.java
@@ -2,9 +2,9 @@ class Solution {
     public int minAddToMakeValid(String s) {
 
         int open = 0;
-        int insertions = 0;
+        int insertions =0;
 
-        for(char ch: s.toCharArray()){
+        for(char ch : s.toCharArray()){
             if(ch == '('){
                 open++;
             }else if(ch == ')'){
