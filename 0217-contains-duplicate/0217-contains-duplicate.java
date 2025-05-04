b4c111875444e83af1b4e9fbe8e3486b686c3b4d
karthickprabakaran <166775653+karthickprabakaran@users.noreply.github.com>
2025-05-04 20:17:38 +0530

    Time: 13 ms (54.4%), Space: 58.3 MB (42.4%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -4,9 +4,11 @@ class Solution {
         HashSet<Integer> set = new HashSet<>();
 
         for(int i=0;i<nums.length;i++){
+
             if(set.contains(nums[i])){
                 return true;
             }
+
             else{
                 set.add(nums[i]);
             }
