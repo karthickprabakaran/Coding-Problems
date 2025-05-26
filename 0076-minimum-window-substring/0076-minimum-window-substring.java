dbb60d0c6ae11e875a72a59c9738938de9cc9a13
karthickprabakaran <166775653+karthickprabakaran@users.noreply.github.com>
2025-05-26 23:52:08 +0530

    Time: 2 ms (99.89%), Space: 44.6 MB (93.8%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,37 +1,38 @@
 class Solution {
     public String minWindow(String s, String t) {
-        if (s.length() < t.length()) return "";
-
-        int[] hash = new int[128]; // ASCII size
-        for (char c : t.toCharArray()) {
-            hash[c]++;
-        }
+        int min = Integer.MAX_VALUE;
+        int sIndex = -1;
+        int cnt = 0;
 
         int left = 0;
-        int right = 0;
-        int count = t.length(); // total chars to match
-        int minLen = Integer.MAX_VALUE;
-        int start = 0;
+        int[] hash = new int[128]; // Assuming ASCII characters
 
-        while (right < s.length()) {
+        for (int i = 0; i < t.length(); i++) {
+            hash[t.charAt(i)]++;
+        }
+
+        for (int right = 0; right < s.length(); right++) {
             char rChar = s.charAt(right);
-            if (hash[rChar] > 0) count--;
-            hash[rChar]--; // Always decrement, even if not in t
-            right++;
+            if (hash[rChar] > 0) {
+                cnt++;
+            }
+            hash[rChar]--;
 
-            while (count == 0) {
-                if (right - left < minLen) {
-                    minLen = right - left;
-                    start = left;
+            while (cnt == t.length()) {
+                if (right - left + 1 < min) {
+                    min = right - left + 1;
+                    sIndex = left;
                 }
 
                 char lChar = s.charAt(left);
                 hash[lChar]++;
-                if (hash[lChar] > 0) count++; // Needed char removed
+                if (hash[lChar] > 0) {
+                    cnt--;
+                }
                 left++;
             }
         }
 
-        return minLen == Integer.MAX_VALUE ? "" : s.substring(start, start + minLen);
+        return sIndex == -1 ? "" : s.substring(sIndex, sIndex + min);
     }
 }
