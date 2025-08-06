92565dae8855433d100a7600ec76159bee2573e4
Karthick Prabakaran <166775653+karthickprabakaran@users.noreply.github.com>
2025-08-06 07:57:47 +0530

    Time: 31 ms (65.97%), Space: 67.6 MB (56.97%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.js
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -1,15 +1,19 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
 var groupAnagrams = function(strs) {
-    const map = new Map();
-    
-    for (let str of strs) {
-        const sortedStr = str.split('').sort().join('');
-        
-        if (!map.has(sortedStr)) {
-            map.set(sortedStr, []);
-        }
-        
+
+    let map = new Map();
+
+    for(let str of strs){
+        let sortedStr = str.split('').sort().join('');
+
+        if(!map.has(sortedStr)) map.set(sortedStr, []);
+
         map.get(sortedStr).push(str);
     }
-    
+
     return Array.from(map.values());
+    
 };
