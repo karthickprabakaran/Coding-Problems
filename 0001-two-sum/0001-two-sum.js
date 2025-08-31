73990a1b4e90bc3c531b247072591fea1258f789
Karthick Prabakaran <166775653+karthickprabakaran@users.noreply.github.com>
2025-08-31 20:26:29 +0530

    Time: 4 ms (55.78%), Space: 55.4 MB (35.1%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -1,16 +1,21 @@
-var twoSum = function(nums, target) {
-  let n = nums.length;
+    /**
+    * @param {number[]} nums
+    * @param {number} target
+    * @return {number[]}
+    */
+    var twoSum = function(nums, target) {
 
-  let map = new Map();
+        let map = new Map();
 
-  for (let i = 0; i < n; i++) {
-    let diff = target - nums[i];
+        for(let i=0;i<nums.length;i++){
+            let comp = target - nums[i];
 
-    if (map.has(diff)) return [map.get(diff),i];
+            if(map.has(comp)){
+                return [map.get(comp), i];
+            }
 
-    map.set(nums[i], i);
+            map.set(nums[i], i);
+        }
 
-  }
-  return [-1, -1];
-
-};
+        return [-1,-1];
+    };
