f771523f977761760003c9e439d7a1f78099eb9a
Karthick Prabakaran <166775653+karthickprabakaran@users.noreply.github.com>
2025-09-01 15:48:40 +0530

    Time: 0 ms (100%), Space: 54.9 MB (15.58%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.js
+++ 0075-sort-colors/0075-sort-colors.js
@@ -4,6 +4,24 @@
  */
 var sortColors = function(nums) {
 
-    nums.sort();
+    let start = 0;
+    let mid = 0;
+    let end = nums.length - 1;
+
+    while(mid <= end){
+        if(nums[mid] == 0){
+            let temp = nums[mid];
+            nums[mid] = nums[start];
+            nums[start] = temp;
+            start++;
+            mid++;
+        }else if(nums[mid] == 1) mid++;
+        else{
+            let temp = nums[mid];
+            nums[mid] = nums[end];
+            nums[end] = temp;
+            end--;
+        }
+    }
     
 };
