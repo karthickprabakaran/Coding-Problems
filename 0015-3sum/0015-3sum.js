5a59de9e8bb3dfa203174497a18fbe8cfb0e0ae8
Karthick Prabakaran <166775653+karthickprabakaran@users.noreply.github.com>
2025-09-01 10:09:27 +0530

    Time: 33 ms (83.12%), Space: 72.4 MB (53.59%) - LeetHub

--- 0015-3sum/0015-3sum.js
+++ 0015-3sum/0015-3sum.js
@@ -1,33 +1,24 @@
-/**
- * @param {number[]} nums
- * @return {number[][]}
- */
 var threeSum = function(nums) {
+    nums.sort((a, b) => a - b);
+    let res = [];
 
-    let n = nums.length;
-    let set = new Set();
-    nums.sort((a,b) => a - b)
-    for(let i= 0; i< n-2;i++){
-        if(nums[i] === nums[i-1]) continue;
-        let left = i+1;
-        let right = n-1;
+    for (let i = 0; i < nums.length - 2; i++) {
+        if (i > 0 && nums[i] === nums[i - 1]) continue; // skip duplicates
+        let left = i + 1, right = nums.length - 1;
 
-        while(left < right){
-            let sum = nums[i]+nums[left]+nums[right];
-
-            if(sum < 0){
+        while (left < right) {
+            let sum = nums[i] + nums[left] + nums[right];
+            if (sum < 0) {
                 left++;
-            }else if(sum > 0){
+            } else if (sum > 0) {
                 right--;
-            }else{
-                let list = [nums[i], nums[left], nums[right]];
-                set.add(list.toString());
-                left++;
-                right--;
+            } else {
+                res.push([nums[i], nums[left], nums[right]]);
+                left++; right--;
+                while (left < right && nums[left] === nums[left - 1]) left++;
+                while (left < right && nums[right] === nums[right + 1]) right--;
             }
         }
     }
-
-    return [...set].map(str => str.split(',').map(Number));
-    
+    return res;
 };
