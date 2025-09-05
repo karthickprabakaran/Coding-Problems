329bd6e7818d68cc46f68360ae06cfe8a6a16741
Karthick Prabakaran <166775653+karthickprabakaran@users.noreply.github.com>
2025-09-05 16:51:25 +0530

    Time: 6 ms (51.36%), Space: 59.4 MB (38.63%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -3,24 +3,17 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
-
+    
     let left = 0;
     let right = s.length -1;
 
     while(left < right){
-        while(left < right && !isAlphaNumeric(s[left])) left++;
+        while(left < right && !/[a-z0-9]/i.test(s[left])) left++;
+        while(left < right && !/[a-z0-9]/i.test(s[right])) right--;
 
-        while(left < right && !isAlphaNumeric(s[right])) right--;
-
-        if(s[left].toLowerCase() != s[right].toLowerCase()) return false;
-
+        if(s[left].toLowerCase() !== s[right].toLowerCase()) return false;
         left++;
         right--;
     }
     return true;
-    
 };
-
-function isAlphaNumeric(char){
-    return /^[a-z0-9]$/i.test(char);
-}
