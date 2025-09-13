db3f098f2c984a7a03a142c1dfb263a8a75f78b4
Karthick Prabakaran <166775653+karthickprabakaran@users.noreply.github.com>
2025-09-13 12:35:26 +0530

    Time: 13 ms (65.75%), Space: 59.6 MB (26.49%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -1,21 +1,21 @@
-var isAnagram = function(s, t) {
+    var isAnagram = function(s, t) {
 
 
 
-  let arr = new Array(26).fill(0);
+    let arr = new Array(26).fill(0);
 
-  for (let char of s) {
-    arr[char.charCodeAt(0) - "a".charCodeAt(0)]++;
-  }
+    for (let char of s) {
+        arr[char.charCodeAt(0) - "a".charCodeAt(0)]++;
+    }
 
-  for (let char of t) {
-    arr[char.charCodeAt(0) - "a".charCodeAt(0)]--;
-  }
+    for (let char of t) {
+        arr[char.charCodeAt(0) - "a".charCodeAt(0)]--;
+    }
 
-  for (let num of arr) {
-    if (num !== 0) return false;
-  }
+    for (let num of arr) {
+        if (num !== 0) return false;
+    }
 
-  return true;
+    return true;
 
-};
+    };
