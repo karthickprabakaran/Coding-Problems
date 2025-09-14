5e9618d6d4d78a183f4fa7c65089093d0d65aa7c
Karthick Prabakaran <166775653+karthickprabakaran@users.noreply.github.com>
2025-09-14 18:19:53 +0530

    Time: 63 ms (11.04%), Space: 59 MB (69.23%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.js
+++ 0141-linked-list-cycle/0141-linked-list-cycle.js
@@ -11,18 +11,16 @@
  * @return {boolean}
  */
 var hasCycle = function(head) {
-
+   
+   if(head == null) return false;
+   
     let slow = head;
-    let fast = head;
+  let fast = head;
 
-    while(fast !== null && fast.next !== null){
-        fast = fast.next.next;
-        slow = slow.next;
-          if(fast == slow){
-    return true;
+  while (fast.next !== null && fast.next.next !== null) {
+    fast = fast.next.next;
+    slow = slow.next;
+    if (slow === fast) return true;
   }
-    }
-  
-    return false;
-  
+  return false;
 };
