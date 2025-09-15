294e0dc5f3263d64fe60ce7307d1306a5ac267fd
Karthick Prabakaran <166775653+karthickprabakaran@users.noreply.github.com>
2025-09-15 07:06:07 +0530

    Time: 3 ms (36.9%), Space: 55.5 MB (65.89%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
@@ -4,22 +4,18 @@
  * @return {number[]}
  */
 var twoSum = function(numbers, target) {
-
     let left = 0;
-    let right = numbers.length -1;
+    let right = numbers.length - 1;
 
-    while(left < right ){
-        let sum = numbers[left]+ numbers[right];
+    while(left < right){
+        let sum = numbers[left] + numbers[right];
         if(sum == target){
             return [left+1, right+1];
-        }
-        else if(sum > target ){
-            right--;
+        }else if(sum > target){
+             right--;
         }else{
             left++;
         }
     }
-
-    return [-1,-1];
-    
+    return [-1, -1];  
 };
