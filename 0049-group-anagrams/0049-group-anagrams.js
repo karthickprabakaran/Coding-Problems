acd7b0ff5ed4263d529e1e1df4b260bb3278bca0
Karthick Prabakaran <166775653+karthickprabakaran@users.noreply.github.com>
2025-10-14 07:39:24 +0530

    Time: 23 ms (90.75%), Space: 68.4 MB (43.61%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.js
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -2,20 +2,25 @@
  * @param {string[]} strs
  * @return {string[][]}
  */
-var groupAnagrams = function(strs) {
+var groupAnagrams = function (strs) {
+  if (strs.length == 0) return strs;
 
-    let map = new Map();
+  let map = new Map();
 
-    for(let str of strs){
-        let sorted = str.split('').sort().join('');
+  for (let str of strs) {
+    let count = new Array(26).fill(0);
 
-        if(!map.has(sorted)){
-                    map.set(sorted, []);
-        }
-            map.get(sorted).push(str);
-
+    for (let char of str) {
+      count[char.charCodeAt(0) - "a".charCodeAt(0)]++;
     }
 
-    return Array.from(map.values());
-    
+    let key = count.join("#");
+
+    if (!map.has(key)) {
+      map.set(key, []);
+    }
+    map.get(key).push(str);
+  }
+
+  return Array.from(map.values());
 };
