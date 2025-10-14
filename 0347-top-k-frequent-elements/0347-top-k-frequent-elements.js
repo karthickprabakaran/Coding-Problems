7c3bf9b05f8cbe5fe36c4fffd2c33ec6e40070fb
Karthick Prabakaran <166775653+karthickprabakaran@users.noreply.github.com>
2025-10-14 09:01:21 +0530

    Time: 15 ms (42.78%), Space: 61.1 MB (35.01%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
@@ -1,28 +1,25 @@
-var topKFrequent = function (nums, k) {
+var topKFrequent = function(nums, k) {
+    let map = new Map();
 
+    // Count frequencies
+    for (let num of nums) {
+        map.set(num, (map.get(num) || 0) + 1);
+    }
 
-let map = new Map();
+    // Initialize buckets: index = frequency
+    let count = new Array(nums.length + 1).fill(0).map(() => []);
 
-for(let num of nums){
-    map.set(num, (map.get(num) || 0) + 1);
-  }
+    for (let [num, freq] of map.entries()) {
+        count[freq].push(num);
+    }
 
-  let freq =  Array.from(map.entries());
+    // Collect top k frequent elements
+    let res = [];
+    for (let i = nums.length; i >= 0 && res.length < k; i--) {
+        if (count[i].length > 0) {
+            res.push(...count[i]);
+        }
+    }
 
-  freq.sort((a,b) => {
-  return (b[1] - a[1]);
-  });
-
-  let res = [];
-
-  for(let i=0;i<k;i++){
-  res.push(freq[i][0]);
-  }
-
-  return res;
-
-
-
-
-
+    return res.slice(0, k); // Just in case we added extra elements
 };
