6889ae6b91cc6f935ef64df00b8d69c7177e3cc8
Karthick Prabakaran <166775653+karthickprabakaran@users.noreply.github.com>
2025-10-15 20:19:13 +0530

    Time: 3 ms (60.56%), Space: 64.2 MB (92.9%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -1,19 +1,14 @@
-/**
- * @param {number[]} prices
- * @return {number}
- */
 var maxProfit = function (prices) {
   let n = prices.length;
-  let max = -Infinity;
+
+  let max = 0;
   let currentLowest = prices[0];
+
   for (let i = 0; i < n; i++) {
     if (prices[i] < currentLowest) currentLowest = prices[i];
-    else {
-      let profit = prices[i] - currentLowest;
-      if (profit > max) max = profit;
-    }
+
+    max = Math.max(max, prices[i] - currentLowest);
   }
 
   return max;
 };
-
