11b706cabfde0f7032b6bbf9c294d4aad76f6010
Karthick Prabakaran <166775653+karthickprabakaran@users.noreply.github.com>
2025-10-16 20:18:59 +0530

    Time: 7 ms (57.54%), Space: 57.6 MB (60.1%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -1,26 +1,19 @@
-/**
- * @param {string} s
- * @return {number}
- */
-var lengthOfLongestSubstring = function(s) {
+var lengthOfLongestSubstring = function (s) {
+  let left = 0;
+  let right = 0;
+  let max = 0;
+  let set = new Set();
 
-    let set = new Set();
-
-    let left = 0;
-    let right = 0;
-    let maxLen = 0;
-
-    while(right < s.length){
-        if(set.has(s.charAt(right))){
-            set.delete(s.charAt(left));
-            left++;
-        }else{
-            set.add(s.charAt(right));
-            maxLen = Math.max(maxLen, right - left +1);
-            right++;
-        }
+  while (right < s.length) {
+    if (!set.has(s[right])) {
+      set.add(s[right]);
+      max = Math.max(max, right - left + 1);
+      right++;
+    } else {
+      set.delete(s[left]);
+      left++;
     }
+  }
 
-    return maxLen;
-    
+  return max;
 };
