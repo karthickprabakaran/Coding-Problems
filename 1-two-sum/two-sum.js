76a3ae0f6d6af83d7c9e91f8e4c85c556e5dcfce
Karthick Prabakaran <166775653+karthickprabakaran@users.noreply.github.com>
2026-07-01 18:52:14 +0530

    Time: 47 ms (14.88%) | Memory: 54.5 MB (65.34%) - LeetSync

---
+++ 1-two-sum/two-sum.js
@@ -0,0 +1,11 @@
+var twoSum = function(nums, target) {
+    let n = nums.length;
+
+    for (let i = 0; i < n; i++) {
+        for (let j = i + 1; j < n; j++) {
+            if (nums[i] + nums[j] === target) {
+                return [i, j];
+            }
+        }
+    }
+};
